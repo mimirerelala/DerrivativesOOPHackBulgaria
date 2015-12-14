e658994d0c3f7a5f51c130c7e96b4d9fe6c9590b
mimirerelala <mirela.napetova@gmail.com>
2015-12-14 19:35:49 +0200

    Classes

--- poly.py
+++ poly.py
@@ -1,9 +1,39 @@
-class Monom():
-    def __init__(self, mono_string):
-        pass
+class Monom:
+    def __init__(self, variable, coefficient, power):
+        self.variable = variable
+        self.coefficient = coefficient
+        self.power = power
 
 
-class Polynom(Monom):
-    def __init__(self, polynomial_string):
-        pass
+    def __add__(self, other_monom ):
+        if (self.variable == other_monom.variable):
+            if (self.power == other_monom.power):
+                return Monom(self.variable, self.coefficient + other_monom.coefficient, self.power)
+
+    def differentiate(self):
+        self.coefficient *= self.power
+        self.power -= 1
+
+
+    def __str__(self):
+        return self.coefficient + "" + self.variable + "^" + self.power
+
+    def __repr__(self):
+        return str(self)
+
+
+
+
+class Polynom:
+    def __init__(self):
+        self.monoms_list = {}
+
+    def add_member(self, monom_member):
+        self.monoms_list[monom_member.power] += monom_member
+
+    def __str__(self):
+        return
+
+    def __repr__(self):
+        return
 
